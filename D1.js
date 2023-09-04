572fa3da01ad222d4e6607859977bc5ee077f0b9
Vincenzo Melillo <melillo_v@yahoo.it>
2023-09-04 15:19:38 +0200

    first commit

---
+++ D1.js
@@ -0,0 +1,71 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+I dattyowe di javascript sono numeri, stringhe (ovvero qualasiasi dato posto fra le due '' ) e boleani ovvero true e false*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 2
+
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name;
+name='Vincenzo';
+
+/* ESERCIZIO 3
+
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x;
+x=21+20;
+console.log(x)
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x;
+x=12;
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+name=Melillo
+const x;
+x=21;
+x=2;
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+let y;
+y=4-x;
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name1;
+let name2;
+name1=john;
+name2=John;
+name1==name2
+false
+name2=john;
+name1=name2 
+true
